0e77b7af340b6b4c231fb0cdec252b7e3a7a47c5
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-06-20 18:57:24 +0500

    add folder db

---
+++ db/folder.ts
@@ -0,0 +1,18 @@
+import { Db } from 'mongodb'
+import { nanoid } from 'nanoid'
+
+export const getFolders = async (db: Db, userId: string) => {
+  return db.collection('folders').find({ createdBy: userId }).toArray()
+}
+
+export const createFolder = async (db: Db, folder: { createdBy: string; name: string }) => {
+  const newFolder = await db
+    .collection('folders')
+    .insertOne({
+      _id: nanoid(),
+      ...folder,
+      createdAt: new Date().toDateString(),
+    })
+    .then(({ ops }) => ops[0])
+  return newFolder
+}
